dc1512c6295e88f245ef96958759bd7f8ad5e5d1
Rajiv Govindjee <rajivg@umich.edu>
2023-04-17 19:12:00 -0400

    Fix and rename FOV test

--- src/test_fov_polyhedron.jl
+++ src/test_fov_polyhedron.jl
@@ -1,13 +1,18 @@
-# Test functions for mapping safe flight polyhedra
+# Test for visualizing camera field of view under rototranslation
+
 include("mapping.jl")
+include("utils.jl")  # For types
+
 using Rotations
 # K from ETH3D dataset example
-K = [726.301    0.0    356.692;
+K = SM3{Float32}([726.301    0.0    356.692;
    0.0    726.301  186.454;
-   0.0      0.0      1.0]
+   0.0      0.0      1.0])
+# Image pixel index rages
 xrange = [0, 458]
 yrange = [0, 739]
-t = [0; 0; 0.0]
+
+t = [0; 0; 0.0]  # Starting translation [m]
 
 if !@isdefined vis
     vis = Visualizer()  # Only need to set up once
@@ -19,7 +24,7 @@ for i in 1:3:200
     global t = [-cos(0.01*i); -sin(0.01*i); 0.002*i]
     #t = [0; 0; 0.0]
     R = RotXYZ(pi/2, 0.01*i, pi/2 + 0.2 - 0.001*i)
-    local T = [R t; 0 0 0 1]  # Camera pose in world frame
+    local T = SM4{Float32}([R t; 0 0 0 1])  # Camera pose in world frame
     plot_fov_polyhedron!(vis, K, inv(T), xrange, yrange)
     sleep(0.1)
 end
